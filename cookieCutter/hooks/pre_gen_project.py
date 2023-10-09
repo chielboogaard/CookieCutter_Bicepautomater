bc28c44a1fe27b7912e0fe4a18288190a0f56ab4
Chiel van den Boogaard <chiel.vandenboogaard@wearetriple.com>
2023-10-09 20:38:00 +0200

    basic pre and post gen scripts for cookiecutter

---
+++ cookieCutter/hooks/pre_gen_project.py
@@ -0,0 +1,9 @@
+import sys, os, shutil
+from cookiecutter.main import cookiecutter
+
+def main():
+    print("********** PRE GENERATION HOOK *************")
+    print("Nothing here yet")
+
+if __name__ == '__main__':
+    sys.exit(main())
