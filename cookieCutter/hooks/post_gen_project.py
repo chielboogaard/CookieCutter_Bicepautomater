319667edb85b54d3eaf240dcfad8f4571f7ca134
Chiel van den Boogaard <chiel.vandenboogaard@wearetriple.com>
2023-10-17 21:09:51 +0200

    copy modules

--- cookieCutter/hooks/post_gen_project.py
+++ cookieCutter/hooks/post_gen_project.py
@@ -1,9 +1,34 @@
 import sys, os, shutil
 from cookiecutter.main import cookiecutter
 
+
 def main():
     print("********** PRE GENERATION HOOK *************")
-    print("Nothing here yet")
+    curdir = os.path.abspath(os.path.curdir)
 
-if __name__ == '__main__':
+    # copy modules
+    framework = "{{ cookiecutter.framework }}"
+    database = "{{ cookiecutter.database}}"
+    if framework != "":
+        get_modules("framework", framework)
+    if database != "":
+        get_modules('database', database)
+
+
+def get_modules(kind, module):
+    modules = []
+    infile = open(f"templates/{kind}/{module}.txt", "r")
+    for line in infile:
+        line = line.strip()
+        modules.append(line)
+    infile.close()
+    copy_modules(modules)
+
+
+def copy_modules(modules):
+    for module in modules:
+        shutil.move(f"bicepModules/{module}", "bicep/modules/")
+
+
+if __name__ == "__main__":
     sys.exit(main())
