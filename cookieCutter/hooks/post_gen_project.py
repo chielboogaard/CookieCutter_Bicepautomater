0adcf9c9739f36d0f52f376eac7209ed4968705e
Chiel van den Boogaard <chiel.vandenboogaard@wearetriple.com>
2023-10-18 20:43:38 +0200

    remove temp folders + database and modules param

--- cookieCutter/hooks/post_gen_project.py
+++ cookieCutter/hooks/post_gen_project.py
@@ -8,11 +8,15 @@ def main():
 
     # copy modules
     framework = "{{ cookiecutter.framework }}"
-    database = "{{ cookiecutter.database}}"
+    database = "{{ cookiecutter.database }}"
+    modules = "{{ cookiecutter.modules }}"
     if framework != "":
         get_modules("framework", framework)
     if database != "":
-        get_modules('database', database)
+        get_modules("database", database)
+    if modules == "yes":
+        print("yesyes")
+    return
 
 
 def get_modules(kind, module):
@@ -23,12 +27,20 @@ def get_modules(kind, module):
         modules.append(line)
     infile.close()
     copy_modules(modules)
+    return
 
 
 def copy_modules(modules):
     for module in modules:
         shutil.move(f"bicepModules/{module}", "bicep/modules/")
+    return
+
+
+def remove_temp_folders():
+    shutil.rmtree("bicepModules")
+    shutil.rmtree("templates")
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
+    sys.exit(remove_temp_folders())
